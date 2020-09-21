147ff1e0dd6c024fdef1b47edde619e576cd988a
Valeriano <valeriano.tpc@gmail.com>
2020-09-21 20:43:42 -0300

    resoluçao

--- result/cesar.js
+++ result/cesar.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const readline = require('readline');
 
    
-async function Leitura() {
+async function Cesar() {
     const fileStream = fs.createReadStream('../inputs/cesar.txt');
     const rl = readline.createInterface({
       input: fileStream,
       crlfDelay: Infinity
     });
     const texto = [];
-    let letras;
+    let letras = 0;
 
     for await (const line of rl) {
         if(!isNaN(line)=== true){
@@ -21,14 +21,23 @@ async function Leitura() {
 
     for(const res of texto){
       let num = 0;
+      let cesar = "";
       for(let i =0;i<res.length;i++){
         if(res.charCodeAt(i) >= 65 && res.charCodeAt(i) <= 90){
-          num = (res.charCodeAt(i) - 65 + 3)% 26
+          num = (res.charCodeAt(i) - 65 + Number(letras))% 26;
+          cesar = cesar +  String.fromCharCode(num+65);
+        }else if(res.charCodeAt(i) >= 97 && res.charCodeAt(i) <= 122){
+          num = (res.charCodeAt(i) - 97 + Number(letras))% 26;
+          cesar = cesar +  String.fromCharCode(num+97);
         }
-        console.log(num + 65)
+        else{
+          cesar = cesar + res.charAt(i);
+        }
       }
+      console.log(cesar);
     }
 
 }
-Leitura();
+
+Cesar();
 
